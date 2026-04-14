b67cfa975662e84adc7dc18a2132668d42ab1cbe
ddonofrio03 <ddonofrio@thecaseygroup.us>
2026-04-14 10:47:58 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SITE_CONFIG = {
+  siteName: "DONOFR.IO",
+  siteTagline: "A family of builders",
+  supabaseUrl: "YOUR_SUPABASE_URL",
+  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  adminEmail: "ddonofrio@thecaseygroup.us"
+};
